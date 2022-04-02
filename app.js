40b60366ea02f98899e2feedb0c4fa0156a5311c
Andres Rodriguez <rodriguezandresed@gmail.com>
2022-04-02 19:30:42 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+
+for(i=0; i<pronoun.length; i++){
+for (j=0; j< adj.length; j++){
+for (k=0; k < noun.length; k++){
+
+    console.log(pronoun[i] + adj[j] + noun[k]+".com")  
+}
+
+}
+
+}
